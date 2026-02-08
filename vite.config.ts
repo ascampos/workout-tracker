fbedcd3d8adf204f1ecfd7460c652d81f1b6b582
Arturo Campos <ascampos@alum.mit.edu>
2026-02-08 03:49:18 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
@@ -16,6 +17,7 @@ export default defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
